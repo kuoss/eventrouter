f244b87dfaea597126cd9e09aa9ba0216c99d6d3
Jmnote <opcore@gmail.com>
2023-11-22 07:20:13 +0000

    bump for security

---
+++ sinks/eventhub_test.go
@@ -0,0 +1,31 @@
+package sinks
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewEventHubSink(t *testing.T) {
+	testCases := []struct {
+		connString string
+		overflow   bool
+		bufferSize int
+		want       *EventHubSink
+		wantError  string
+	}{
+		{"", false, 0, nil, `failed parsing connection string due to unmatched key value separated by '='`},
+	}
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) {
+			got, err := NewEventHubSink(tc.connString, tc.overflow, tc.bufferSize)
+			if tc.wantError == "" {
+				require.NoError(t, err)
+				require.NotEmpty(t, got)
+			} else {
+				require.EqualError(t, err, tc.wantError)
+				require.Nil(t, got)
+			}
+		})
+	}
+}
