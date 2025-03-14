88562715811aed0534aeecc1ba41cfcaa0d00c99
Jmnote <opcore@gmail.com>
2025-03-15 03:40:54 +0900

    bump libs (#33)
    * bump libs

--- main_test.go
+++ main_test.go
@@ -8,6 +8,6 @@ import (
 
 func TestLoadConfig(t *testing.T) {
 	k8s, err := loadConfig()
-	require.EqualError(t, err, `ReadInConfig err: While parsing config: invalid character '"' after object key:value pair`)
+	require.EqualError(t, err, "BuildConfigFromFlags err: stat /var/run/kubernetes/admin.kubeconfig: no such file or directory")
 	require.Nil(t, k8s)
 }
