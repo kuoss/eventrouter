40f4cec2056061996acce388d4760b316e3b29c4
jmnote <opcore@gmail.com>
2025-03-16 04:00:08 +0900

    remove heavy libs

--- sinks/interfaces.go
+++ sinks/interfaces.go
@@ -82,59 +82,6 @@ func ManufactureSink() (e EventSinkInterface) {
 			panic(err.Error())
 		}
 		return e
-	case "s3sink":
-		accessKeyID := viper.GetString("s3SinkAccessKeyID")
-		if accessKeyID == "" {
-			panic("s3 sink specified but s3SinkAccessKeyID not specified")
-		}
-
-		secretAccessKey := viper.GetString("s3SinkSecretAccessKey")
-		if secretAccessKey == "" {
-			panic("s3 sink specified but s3SinkSecretAccessKey not specified")
-		}
-
-		region := viper.GetString("s3SinkRegion")
-		if region == "" {
-			panic("s3 sink specified but s3SinkRegion not specified")
-		}
-
-		bucket := viper.GetString("s3SinkBucket")
-		if bucket == "" {
-			panic("s3 sink specified but s3SinkBucket not specified")
-		}
-
-		bucketDir := viper.GetString("s3SinkBucketDir")
-		if bucketDir == "" {
-			panic("s3 sink specified but s3SinkBucketDir not specified")
-		}
-
-		// By default the json is pushed to s3 in not flatenned rfc5424 write format
-		// The option to write to s3 is in the flattened json format which will help in
-		// using the data in redshift with least effort
-		viper.SetDefault("s3SinkOutputFormat", "rfc5424")
-		outputFormat := viper.GetString("s3SinkOutputFormat")
-		if outputFormat != "rfc5424" && outputFormat != "flatjson" {
-			panic("s3 sink specified, but incorrect s3SinkOutputFormat specified. Supported formats are: rfc5424 (default) and flatjson")
-		}
-
-		// By default we buffer up to 1500 events, and drop messages if more than
-		// 1500 have come in without getting consumed
-		viper.SetDefault("s3SinkBufferSize", 1500)
-		viper.SetDefault("s3SinkDiscardMessages", true)
-
-		viper.SetDefault("s3SinkUploadInterval", 120)
-		uploadInterval := viper.GetInt("s3SinkUploadInterval")
-
-		bufferSize := viper.GetInt("s3SinkBufferSize")
-		overflow := viper.GetBool("s3SinkDiscardMessages")
-
-		s, err := NewS3Sink(accessKeyID, secretAccessKey, region, bucket, bucketDir, uploadInterval, overflow, bufferSize, outputFormat)
-		if err != nil {
-			panic(err.Error())
-		}
-
-		go s.Run(make(chan bool))
-		return s
 	case "influxdb":
 		host := viper.GetString("influxdbHost")
 		if host == "" {
@@ -188,24 +135,6 @@ func ManufactureSink() (e EventSinkInterface) {
 			panic(err.Error())
 		}
 		return influx
-	case "eventhub":
-		connString := viper.GetString("eventHubConnectionString")
-		if connString == "" {
-			panic("eventhub sink specified but eventHubConnectionString not specified")
-		}
-		// By default we buffer up to 1500 events, and drop messages if more than
-		// 1500 have come in without getting consumed
-		viper.SetDefault("eventHubSinkBufferSize", 1500)
-		viper.SetDefault("eventHubSinkDiscardMessages", true)
-
-		bufferSize := viper.GetInt("eventHubSinkBufferSize")
-		overflow := viper.GetBool("eventHubSinkDiscardMessages")
-		eh, err := NewEventHubSink(connString, overflow, bufferSize)
-		if err != nil {
-			panic(err.Error())
-		}
-		go eh.Run(make(chan bool))
-		return eh
 	// case "logfile"
 	default:
 		err := errors.New("invalid Sink Specified")
